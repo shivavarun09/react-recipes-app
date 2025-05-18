ab053633047253e1860320b2645e281568a2b32a
shivavarun09 <varunapps09@gmail.com>
2025-05-18 14:13:06 +0530

    msg

--- src/Componets/Home/Home.jsx
+++ src/Componets/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div style={{display:"flex",justifyContent:"center",alignItems:"center",height:"80vh"}}>
-      <h1 style={{fontFamily:"-moz-initial",color:"green"}}>This is Home Page Click On View All Recipes</h1>
+      <h1 style={{fontFamily:"-moz-initial",color:"green",textAlign:"center"}}>This is Home Page Click On View All Recipes</h1>
     </div>
   )
 }
