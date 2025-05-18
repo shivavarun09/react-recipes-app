166d037d7f97a0bea837408b6e7014154e65d416
shivavarun09 <varunapps09@gmail.com>
2025-05-18 19:29:06 +0530

    msg

--- src/Componets/Footer/Footer.jsx
+++ src/Componets/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footerDiv'>
-      <p style={{display:"block"}}>© 2025 RecipesApp — Designed & Developed with ❤️ by Shivavarun.</p> <br />
+      <p>© 2025 RecipesApp — Designed & Developed with ❤️ by Shivavarun.</p> <br />
       
     </div>
   )
